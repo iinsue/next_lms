a3230936599c3932b7377fdd156fb561b80624c8
iinsue <hinsu93@naver.com>
2023-09-30 19:14:19 +0900

    Grouping Route Test

---
+++ app/(group)/routea/page.tsx
@@ -0,0 +1,9 @@
+const RouteA = () => {
+  return (
+    <>
+      <div>This is a route A inside a group folder </div>
+    </>
+  );
+};
+
+export default RouteA;
---
+++ app/(group)/routeb/page.tsx
@@ -0,0 +1,9 @@
+const RouteB = () => {
+  return (
+    <>
+      <div>This is a route B inside a group folder </div>
+    </>
+  );
+};
+
+export default RouteB;
