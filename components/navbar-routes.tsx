179c68daac0d6d2246e1812afe50c56249dad838
iinsue <hinsu93@naver.com>
2023-09-30 23:13:52 +0900

    Teacher 모드 기능 구현

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -10,7 +10,6 @@ import Link from "next/link";
 
 export const NavbarRoutes = () => {
   const pathname = usePathname();
-  const router = useRouter();
 
   const isTeacherPage = pathname?.startsWith("/teacher");
   const isPlayerPage = pathname?.includes("/chapter");
@@ -19,10 +18,12 @@ export const NavbarRoutes = () => {
     <>
       <div className="flex gap-x-2 ml-auto">
         {isTeacherPage || isPlayerPage ? (
-          <Button size="sm" variant="ghost">
-            <LogOut />
-            Exit
-          </Button>
+          <Link href="/">
+            <Button size="sm" variant="ghost">
+              <LogOut className="h-4 w-4 mr-2" />
+              Exit
+            </Button>
+          </Link>
         ) : (
           <Link href="/teacher/courses">
             <Button size="sm" variant="ghost">
