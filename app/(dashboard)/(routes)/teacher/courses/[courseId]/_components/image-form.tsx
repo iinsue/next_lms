4aeb128c9992b18a23260197e1a6594d4764ce19
iinsue <hinsu93@naver.com>
2023-10-02 00:43:29 +0900

    카테고리 선택 기능 추가

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/image-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/image-form.tsx
@@ -45,7 +45,7 @@ export const ImageForm = ({ initialData, courseId }: ImageFormProps) => {
     <>
       <div className="mt-6 border bg-slate-100 rounded-md p-4">
         <div className="font-medium flex items-center justify-between">
-          Course Image
+          Course image
           <Button onClick={toggleEdit} variant="ghost">
             {isEditing && <>Cancel</>}
             {!isEditing && !initialData.imageUrl && (
