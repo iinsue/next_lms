ea07300a7f6e446e6903a9e7c13b5e3ceeaad7bc
iinsue <hinsu93@naver.com>
2023-10-01 19:53:04 +0900

    Course Title 입력 폼 생성

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -3,6 +3,7 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { LayoutDashboard } from "lucide-react";
 import { redirect } from "next/navigation";
+import { TitleForm } from "./_components/title-form";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const { userId } = auth();
@@ -51,6 +52,7 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
               <IconBadge variant="success" size="sm" icon={LayoutDashboard} />
               <h2 className="text-xl">Customize your course</h2>
             </div>
+            <TitleForm initialData={course} courseId={course.id} />
           </div>
         </div>
       </div>
