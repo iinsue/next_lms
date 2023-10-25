e8ad3dc8eb918fb014a14680d4bbe2a53dc9df33
Insu Hong <hinsu93@gmail.com>
2023-10-25 18:59:48 +0900

    Chapter 설정 불러오는 Action 추가

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,7 +1,62 @@
-const ChapterIdPage = () => {
+import { getChapter } from "@/actions/get-chapter";
+import { Banner } from "@/components/banner";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+const ChapterIdPage = async ({
+  params,
+}: {
+  params: { courseId: string; chapterId: string };
+}) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const {
+    chapter,
+    course,
+    muxData,
+    attachments,
+    nextChapter,
+    userProgress,
+    purchase,
+  } = await getChapter({
+    userId,
+    chapterId: params.chapterId,
+    courseId: params.courseId,
+  });
+
+  if (!chapter || !course) {
+    return redirect("/");
+  }
+
+  const isLocked = !chapter.isFree && !purchase;
+  const completeOnEnd = !!purchase && !userProgress?.isCompleted;
+
   return (
     <>
-      <div>Chapter Id!</div>
+      {userProgress?.isCompleted && (
+        <Banner variant="success" label="You already completed this chapter." />
+      )}
+      {isLocked && (
+        <Banner
+          variant="warning"
+          label="You need to purchase this course to watch this chapter."
+        />
+      )}
+      <div className="flex flex-col max-w-4xl mx-auto pb-20">
+        {/* <VideoPlayer
+          chapterId={params.chapterId}
+          title={chapter.title}
+          courseId={params.courseId}
+          nextChapterId={nextChapter?.id}
+          playbackId={muxData?.playbackId}
+          isLocked={isLocked}
+          completeOnEnd={completeOnEnd}
+        /> */}
+      </div>
     </>
   );
 };
